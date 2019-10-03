b8d42187ddc41749590e3f5c986c59f76b088a54
oathzzh <2017118118@stu.hstc.edu.cn>
2019-10-03 08:36:47 +0800

    singleTask模式

--- Work4/app/src/main/java/com/example/work4/Hello2.java
+++ Work4/app/src/main/java/com/example/work4/Hello2.java
@@ -4,15 +4,22 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 
 public class Hello2 extends AppCompatActivity {
+    private static final String TAG="Hello2";
+    private static int objCount=0;
+    private int mobjCount=0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_hello2);
+        objCount++;
+        mobjCount=objCount;
+        Log.d("Hello2",this.toString());
         Button button2 = (Button)findViewById(R.id.button_2);
         button2.setOnClickListener(new View.OnClickListener(){
             @Override
@@ -21,5 +28,40 @@ public class Hello2 extends AppCompatActivity {
                 startActivity(intent);
             }
         });
+    }
+    @Override
+    protected void onStart() {
+        super.onStart();
+        Log.d(TAG, "onStart: "+mobjCount);
+    }
+
+    @Override
+    protected void onResume(){
+        super.onResume();
+        Log.d(TAG, "onResume: "+mobjCount);
+    }
+
+    @Override
+    protected  void onPause(){
+        super.onPause();
+        Log.d(TAG, "onPause: "+mobjCount);
+    }
+
+    @Override
+    protected  void onStop(){
+        super.onStop();
+        Log.d(TAG, "onStop: "+mobjCount);
+    }
+
+    @Override
+    protected void onDestroy(){
+        super.onDestroy();
+        Log.d(TAG, "onDestroy: "+mobjCount);
+    }
+
+    @Override
+    protected void onRestart(){
+        super.onRestart();
+        Log.d(TAG, "onRestart: "+mobjCount);
     }
 }
