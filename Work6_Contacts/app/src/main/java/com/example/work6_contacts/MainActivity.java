5b9789e4a0ac0493882c4e7118540f52d0cb4cf3
oathzzh <2017118118@stu.hstc.edu.cn>
2019-11-04 21:21:56 +0800

    update layout and add function

--- Work6_Contacts/app/src/main/java/com/example/work6_contacts/MainActivity.java
+++ Work6_Contacts/app/src/main/java/com/example/work6_contacts/MainActivity.java
@@ -3,9 +3,11 @@ package com.example.work6_contacts;
 import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
 import android.content.DialogInterface;
 import android.os.Bundle;
 import android.provider.ContactsContract;
+import android.text.TextUtils;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
@@ -14,60 +16,112 @@ import android.widget.EditText;
 import android.widget.ListView;
 import android.widget.Toast;
 
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.ArrayList;
+
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
     private EditText editText;
-
-    private String[] data = {"apple:1231251", "Banana:1341325", "Orange:16416537"};
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        ArrayAdapter<String> adapter = new ArrayAdapter<String>(MainActivity.this, android.R.layout.simple_list_item_1, data);
-        final ListView listView = (ListView) findViewById(R.id.list_view);
-        listView.setAdapter(adapter);
+        editText = (EditText) findViewById(R.id.edit_text);
+        String inputText = load();
+        if (!TextUtils.isEmpty(inputText)) {
+            editText.setText(inputText);
+            editText.setSelection(inputText.length());
+        }
         Button button = (Button) findViewById(R.id.add);
         Button button1 = (Button) findViewById(R.id.delete);
-        Button button2 = (Button) findViewById(R.id.update);
-        editText = (EditText) findViewById(R.id.edit_text);
         button.setOnClickListener(this);
         button1.setOnClickListener(this);
-        button2.setOnClickListener(this);
+    }
+
+    public String load() {
+        FileInputStream in = null;
+        BufferedReader reader = null;
+        StringBuilder content = new StringBuilder();
+        try {
+            in = openFileInput("data");
+            reader = new BufferedReader(new InputStreamReader(in));
+            String line = "";
+            while ((line = reader.readLine()) != null) {
+                content.append(line);
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            if (reader != null) {
+                try {
+                    reader.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+        return content.toString();
+    }
+
+    public void save(String inputText) {
+        FileOutputStream out = null;
+        BufferedWriter writer = null;
+        try {
+            out = openFileOutput("data", Context.MODE_PRIVATE);
+            writer = new BufferedWriter(new OutputStreamWriter(out));
+            writer.write(inputText);
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            try {
+                if (writer != null) {
+                    writer.close();
+                }
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
     }
 
     @Override
     public void onClick(View v) {
         switch (v.getId()) {
             case R.id.add:
-                String inputText = editText.getText().toString();
+                final String inputText = editText.getText().toString();
                 if (inputText.isEmpty()) {
                     Toast.makeText(MainActivity.this, "您未输入任何信息", Toast.LENGTH_SHORT).show();
-                } else
-                    Toast.makeText(MainActivity.this, inputText + "已添加到通讯录", Toast.LENGTH_SHORT).show();
+                } else {
+                    save(inputText);
+                    Toast.makeText(MainActivity.this, inputText + "已保存", Toast.LENGTH_SHORT).show();
+                }
                 break;
             case R.id.delete:
                 AlertDialog.Builder dialog = new AlertDialog.Builder(MainActivity.this);
-                dialog.setTitle("警告！");
-                dialog.setMessage("你确定要删除此联系人吗？");
+                dialog.setTitle("删除联系人！");
+                dialog.setMessage("重启应用生效");
                 dialog.setCancelable(false);
                 dialog.setPositiveButton("删除", new DialogInterface.OnClickListener() {
+                    final String inputText = editText.getText().toString();
+
                     @Override
                     public void onClick(DialogInterface dialog, int which) {
-
+                        save("");
                     }
                 });
                 dialog.setNegativeButton("取消", new DialogInterface.OnClickListener() {
                     @Override
                     public void onClick(DialogInterface dialog, int which) {
-
+                        load();
                     }
                 });
                 dialog.show();
-                break;
-            case R.id.update:
-                break;
-            default:
                 break;
         }
     }
